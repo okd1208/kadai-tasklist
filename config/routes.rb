45d1ea12e22d1840e67b5e6965ff6bffb3d13259
yutaro.okada <y.okada.upd@gmail.com>
2020-09-18 10:33:29 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   
+  get 'signup', to: 'users#new'
     resources :tasks
+    
+    get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+    
+    resources :users, only: [:new, :create]
 end
