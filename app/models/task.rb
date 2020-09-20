4a51b1586980183382cb57c84dc1105ab6579651
yutaro.okada <y.okada.upd@gmail.com>
2020-09-20 08:01:15 +0000

    space

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
     belongs_to :user
     validates :content, presence: true, length: { maximum: 255 }
-　  validates :status, presence: true, length: { maximum: 20 }
+    validates :status, presence: true, length: { maximum: 20 }
 end
