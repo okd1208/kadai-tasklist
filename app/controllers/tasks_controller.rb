76b0f5b7f08202e6e2a4b69297eec61be7da5845
yutaro.okada <y.okada.upd@gmail.com>
2020-09-22 16:48:56 +0000

    current_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy, :update, :edit]
   
       def index
           # @tasks = Task.all
@@ -29,11 +30,11 @@ class TasksController < ApplicationController
       end
     
       def edit
-          @task = Task.find(params[:id])
+          # @task = Task.find(params[:id])
       end
     
       def update
-          @task = Task.find(params[:id])
+          # @task = Task.find(params[:id])
 
         if @task.update(task_params)
           flash[:success] = 'タスクは正常に更新されました'
@@ -45,7 +46,6 @@ class TasksController < ApplicationController
       end
     
       def destroy
-           @task = Task.find(params[:id])
             @task.destroy
         
             flash[:success] = 'タスクは正常に削除されました'
@@ -56,5 +56,12 @@ class TasksController < ApplicationController
       
       def task_params
       params.require(:task).permit(:content, :status)
+      end
+      
+      def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+          redirect_to root_url
+        end
       end
 end
